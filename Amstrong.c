d9b431bf866f8cd9abb0345c24f81d3f45ae8dca
Nethreshwar <112603835+Nethreshwar@users.noreply.github.com>
2023-08-01 10:42:36 +0530

    Update Amstrong.c

--- Amstrong.c
+++ Amstrong.c
@@ -1,25 +1,19 @@
-#include <math.h>
-#include <stdio.h>
-int main()
-{
-    int i, sum, num, count = 0;
-    printf("All Armstrong number between 1 and 1000 are:\n");
-    for (i = 1; i <= 1000; i++) 
-	{
-        num = i;
-        while (num != 0) 
-		{
-            num /= 10;
-            count++;
-        }
-        num = i;
-        sum = pow(num % 10, count)
-              + pow((num % 100 - num % 10) / 10, count)
-              + pow((num % 1000 - num % 100) / 100, count);
-        if (sum == i) 
-		{
-            printf("%d ", i);
-        }
-        count = 0;
-    }
-}
+#include<stdio.h>  
+ int main()    
+{    
+int n,r,sum=0,temp;    
+printf("enter the number=");    
+scanf("%d",&n);    
+temp=n;    
+while(n>0)    
+{    
+r=n%10;    
+sum=sum+(r*r*r);    
+n=n/10;    
+}    
+if(temp==sum)    
+printf("armstrong  number ");    
+else    
+printf("not armstrong number");    
+return 0;  
+}   
